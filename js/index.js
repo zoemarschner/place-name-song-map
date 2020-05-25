28bb1a7b95071f87de4f126eeb828601e9d0fbe4
Zoë Marschner <zoe.marschner@gmail.com>
2020-05-25 19:08:55 -0400

    not pat's cat :(

--- js/index.js
+++ js/index.js
@@ -41,6 +41,6 @@ function addimage(filename, coord1, coord2, name, sourcename, id) {
 }
  
 map.on('load', function() {
- addimage('../images/patscat.jpg', 0, 0, 'cat', 'point', 'points');
- addimage('../images/patscat.jpg', 12.550343, 55.665957, 'copenhagencat', 'point2', 'points2');
+ addimage('https://upload.wikimedia.org/wikipedia/commons/7/7c/201408_cat.png', 0, 0, 'cat', 'point', 'points');
+ addimage('https://upload.wikimedia.org/wikipedia/commons/7/7c/201408_cat.png', 12.550343, 55.665957, 'copenhagencat', 'point2', 'points2');
 });
