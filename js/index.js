d36e263a67c7b746ab43db7a50f34f47600bc913
Zoë Marschner <zoe.marschner@gmail.com>
2020-05-25 18:56:25 -0400

    braces

--- js/index.js
+++ js/index.js
@@ -1,41 +1,43 @@
 mapboxgl.accessToken = 'pk.eyJ1Ijoiem9lbWFyc2NobmVyIiwiYSI6ImNrYW16eXdibTB1eDgyenBrZXJkb2txZzUifQ.Xbmkj-0Dqf-e1zn_RWuh-Q';
 var map = new mapboxgl.Map({
-container: 'map',
-style: 'mapbox://styles/mapbox/streets-v11'
+	container: 'map',
+	style: 'mapbox://styles/mapbox/streets-v11'
 });
 
 addimage(filename, coord1, coord2, name, sourcename) {
-map.loadImage(
-filename,
-function(error, image) {
-if (error) throw error;
-map.addImage(name, image);
-map.addSource(sourcename, {
-'type': 'geojson',
-'data': {
-'type': 'FeatureCollection',
-'features': [
-{
-'type': 'Feature',
-'geometry': {
-'type': 'Point',
-'coordinates': [coord1, coord2]
-}
-}
-]
-}
-});
-map.addLayer({
-'id': 'points',
-'type': 'symbol',
-'source': sourcename,
-'layout': {
-'icon-image': name,
-'icon-size': 0.1
-}
-});
-}
-);
+	map.loadImage(filename, function(error, image) {
+		if (error) throw error;
+		map.addImage(name, image);
+		map.addSource(sourcename, {
+			'type': 'geojson',
+			'data': 
+			{
+				'type': 'FeatureCollection',
+				'features': [
+					{
+						'type': 'Feature',
+						'geometry': 
+						{
+							'type': 'Point',
+							'coordinates': [coord1, coord2]
+						}
+					}
+				]
+			}
+		}
+	});
+	map.addLayer(
+	{
+		'id': 'points',
+		'type': 'symbol',
+		'source': sourcename,
+		'layout': 
+		{
+			'icon-image': name,
+			'icon-size': 0.1
+		}
+	});
+
 }
  
 map.on('load', function() {
