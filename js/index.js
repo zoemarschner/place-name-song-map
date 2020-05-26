3352a7d6047c431199aed1f37388cabce150af5c
Zoë Marschner <zoe.marschner@gmail.com>
2020-05-25 23:38:42 -0400

    it works nowgit status

--- js/index.js
+++ js/index.js
@@ -43,9 +43,11 @@ function addimage(filename, coord1, coord2, name, sourcename, id) {
  
 map.on('load', function() {
 	d3.csv("getting_album_covers/song_data.csv").then(function(data) {
-	for (thingy in data) {
+	console.log(data)
+	data.forEach(function(thingy) {
+		console.log(thingy)
 		addimage(thingy.Image, thingy.Lon, thingy.Lat, thingy.Song+thingy.Artist, thingy.Song+thingy.Artist, thingy.Song+thingy.Artist); 
-	};
+	});
 });
 //  addimage('https://upload.wikimedia.org/wikipedia/commons/7/7c/201408_cat.png', 0, 0, 'cat', 'point', 'points');
 //  addimage('https://upload.wikimedia.org/wikipedia/commons/7/7c/201408_cat.png', 12.550343, 55.665957, 'copenhagencat', 'point2', 'points2');
