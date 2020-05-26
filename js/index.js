01f3c509319eab7c89fc4a1e46e7f73f79dd95e4
patriciadgao <51918324+patriciadgao@users.noreply.github.com>
2020-05-25 21:27:21 -0600

    Update index.js

--- js/index.js
+++ js/index.js
@@ -40,12 +40,13 @@ function addimage(filename, coord1, coord2, name, sourcename, id) {
 	});
 	
 }
-
-d3.csv("getting_album_covers/song_data.csv").then(function(data) {
-	console.log(data);
-});
  
 map.on('load', function() {
- addimage('https://upload.wikimedia.org/wikipedia/commons/7/7c/201408_cat.png', 0, 0, 'cat', 'point', 'points');
- addimage('https://upload.wikimedia.org/wikipedia/commons/7/7c/201408_cat.png', 12.550343, 55.665957, 'copenhagencat', 'point2', 'points2');
+	d3.csv("getting_album_covers/song_data.csv").then(function(data) {
+	for (thingy in data) {
+		addimage(thingy.Image, thingy.Lon, thingy.Lat, thingy.Song+thingy.Artist, thingy.Song+thingy.Artist, thingy.Song+thingy.Artist); 
+	};
+});
+//  addimage('https://upload.wikimedia.org/wikipedia/commons/7/7c/201408_cat.png', 0, 0, 'cat', 'point', 'points');
+//  addimage('https://upload.wikimedia.org/wikipedia/commons/7/7c/201408_cat.png', 12.550343, 55.665957, 'copenhagencat', 'point2', 'points2');
 });
