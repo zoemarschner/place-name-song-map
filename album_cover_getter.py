8f0ee32e26a3468a35e0f66979d615de74c4657d
Zoë Marschner <zoe.marschner@gmail.com>
2020-05-25 22:02:23 -0400

    :)

--- album_cover_getter.py
+++
@@ -1,8 +0,0 @@
-import spotipy
-from spotipy.oauth2 import SpotifyClientCredentials
-
-sp = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
-
-img = sp.album("3prBy9ITryUQUYKuUTn4EM")
-
-print(img)
